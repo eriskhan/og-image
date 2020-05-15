770657c87f3a9f8d0e5faa14689e49844fd31c57
eriskhan <erik@geshaev.eu>
2020-05-16 01:03:36 +0300

    Update index.ts

--- web/index.ts
+++ web/index.ts
@@ -142,16 +142,16 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Vercel', value: 'https://diktator.media/wp-content/uploads/2020/05/favicon-3.png' },
+    { text: 'Next.js', value: 'https://diktator.media/wp-content/uploads/2020/05/favicon-3.png' },
+    { text: 'Hyper', value: 'https://diktator.media/wp-content/uploads/2020/05/favicon-3.png' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Vercel', value: 'https://diktator.media/wp-content/uploads/2020/05/favicon-3.png' },
+    { text: 'Next.js', value: 'https://diktator.media/wp-content/uploads/2020/05/favicon-3.png' },
+    { text: 'Hyper', value: 'https://diktator.media/wp-content/uploads/2020/05/favicon-3.png' },
 ];
 
 const widthOptions = [
